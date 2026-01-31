75ebcbe8e2d33ac451cef83d18f308a0394f3a9a
lokesh <kagathiloki1122@gmail.com>
2026-01-31 15:23:34 +0530

    Add files via upload

---
+++ Arya.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Arya{
+    public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter your age");
+    int age=sc.nextInt();
+    System.out.println(age);
+    }
+}
